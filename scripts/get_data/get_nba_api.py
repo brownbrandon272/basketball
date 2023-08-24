59afad6c9a7fa68dfefc06dd93e95fbf1fc69c96
Brandon <brownbrandon272@gmail.com>
2023-08-24 17:13:46 +0100

    black formatting and copy config.yml from deploying-ml-models repo

--- scripts/get_data/get_nba_api.py
+++ scripts/get_data/get_nba_api.py
@@ -56,13 +56,17 @@ def retry_api_call(api_call, max_retries=MAX_RETRIES):
 def get_draft_history(season_year=None):
     if season_year is None:
         return drafthistory.DraftHistory().draft_history.get_data_frame()
-    return drafthistory.DraftHistory(season_year_nullable=season_year).draft_history.get_data_frame()
+    return drafthistory.DraftHistory(
+        season_year_nullable=season_year
+    ).draft_history.get_data_frame()
 
 
 ## APIs by season
 # Get seasons of game log data
 def get_leaguegamelog_data(year, season_type_all_star):
-    lg = leaguegamelog.LeagueGameLog(season=str(year), season_type_all_star=season_type_all_star)
+    lg = leaguegamelog.LeagueGameLog(
+        season=str(year), season_type_all_star=season_type_all_star
+    )
     data_frame = lg.league_game_log.get_data_frame()
     data_frame["SeasonTypeAllStar"] = season_type_all_star
     return data_frame
@@ -84,7 +88,8 @@ def fetch_game_data():
         print(f"Getting data for {year}")
         for season_type in season_types:
             game_log_data = pd.concat(
-                [game_log_data, get_leaguegamelog_data(year, season_type)], ignore_index=True
+                [game_log_data, get_leaguegamelog_data(year, season_type)],
+                ignore_index=True,
             )
 
     # Export 'game_log_data' to a single CSV file
@@ -96,7 +101,9 @@ def fetch_game_data():
 ## APIs - game_date
 def fetch_scoreboard_data(game_date) -> t.Tuple[pd.DataFrame, list]:
     def api_call():
-        return scoreboardv2.ScoreboardV2(game_date=game_date).game_header.get_data_frame()
+        return scoreboardv2.ScoreboardV2(
+            game_date=game_date
+        ).game_header.get_data_frame()
 
     data = retry_api_call(api_call)
     game_ids = data["GAME_ID"].unique().tolist()
@@ -108,7 +115,9 @@ def fetch_boxscoretraditional_data(game_ids):
     player_stats_data = pd.DataFrame()
 
     def api_call(game_id):
-        return boxscoretraditionalv2.BoxScoreTraditionalV2(game_id=game_id).player_stats.get_data_frame()
+        return boxscoretraditionalv2.BoxScoreTraditionalV2(
+            game_id=game_id
+        ).player_stats.get_data_frame()
 
     for i, game_id in enumerate(game_ids):
         data = retry_api_call(lambda: api_call(game_id))
@@ -124,7 +133,9 @@ def fetch_boxscoreadvanced_data(game_ids):
     player_stats_data = pd.DataFrame()
 
     def api_call(game_id):
-        return boxscoreadvancedv2.BoxScoreAdvancedV2(game_id=game_id).player_stats.get_data_frame()
+        return boxscoreadvancedv2.BoxScoreAdvancedV2(
+            game_id=game_id
+        ).player_stats.get_data_frame()
 
     for i, game_id in enumerate(game_ids):
         data = retry_api_call(lambda: api_call(game_id))
@@ -154,7 +165,10 @@ def fetch_data():
         box_score_traditional_data = fetch_boxscoretraditional_data(game_ids)
 
         save_data(scoreboard_data, box_score_traditional_data)
-        printer(i=i, verbage=f"Scoreboard and Box Score Data saved for {game_date}, game_ids: {game_ids}")
+        printer(
+            i=i,
+            verbage=f"Scoreboard and Box Score Data saved for {game_date}, game_ids: {game_ids}",
+        )
     return
 
 
@@ -185,19 +199,26 @@ def save_data(scoreboard_data, box_score_traditional_data):
 
     # Read the existing box score traditional CSV if it exists
     try:
-        existing_box_score_traditional_data = pd.read_csv(data_path + f"box_score_traditional_data.csv")
+        existing_box_score_traditional_data = pd.read_csv(
+            data_path + f"box_score_traditional_data.csv"
+        )
     except FileNotFoundError:
         existing_box_score_traditional_data = pd.DataFrame()
 
     # Append 'scoreboard_data' to the existing CSV and export to a new CSV file
-    updated_scoreboard_data = pd.concat([existing_scoreboard_data, scoreboard_data], ignore_index=True)
+    updated_scoreboard_data = pd.concat(
+        [existing_scoreboard_data, scoreboard_data], ignore_index=True
+    )
     updated_scoreboard_data.to_csv(data_path + f"scoreboard_data.csv", index=False)
 
     # Append 'box_score_traditional_data' to the existing CSV and export to a new CSV file
     updated_box_score_traditional_data = pd.concat(
-        [existing_box_score_traditional_data, box_score_traditional_data], ignore_index=True
+        [existing_box_score_traditional_data, box_score_traditional_data],
+        ignore_index=True,
     )
-    updated_box_score_traditional_data.to_csv(data_path + f"box_score_traditional_data.csv", index=False)
+    updated_box_score_traditional_data.to_csv(
+        data_path + f"box_score_traditional_data.csv", index=False
+    )
     return
 
 
