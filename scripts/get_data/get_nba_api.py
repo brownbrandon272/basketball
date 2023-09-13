f6f57cafd40d0cfb3952d66a0abeca4d5eb14506
Brandon Brown <brownbrandon272@gmail.com>
2023-09-13 23:22:20 +0100

    adding option to query recent nba data

--- scripts/get_data/get_nba_api.py
+++ scripts/get_data/get_nba_api.py
@@ -10,7 +10,11 @@ import pandas as pd
 import numpy as np
 
 from scripts.get_data.call_api import retry_api_call
-from scripts.get_data.nba_api_endpoints import SEASONS_DICT, LIST_OF_ENDPOINT_DICTS
+from scripts.get_data.nba_api_endpoints import (
+    SEASONS_DICT,
+    LIST_OF_ALL_ENDPOINT_DICTS,
+    LIST_OF_DATE_ENDPOINT_DICTS,
+)
 
 
 TEST_LOOPS = 2
@@ -21,16 +25,37 @@ def retrieve_data(
     test: bool = False,
     specific_endpoint_names: t.List[str] = None,
 ):
+    if all_dates:
+        _list_of_endpoint_dicts = _check_endpoint_param(
+            LIST_OF_ALL_ENDPOINT_DICTS, specific_endpoint_names
+        )
+    else:
+        _list_of_endpoint_dicts = _check_endpoint_param(
+            LIST_OF_DATE_ENDPOINT_DICTS, specific_endpoint_names
+        )
+
+    for endpoint_dict in _list_of_endpoint_dicts:
+        query_api = NBAAPI(endpoint_dict=endpoint_dict, all_dates=all_dates, test=test)
+        if all_dates:
+            query_api.fetch_all_data()
+        else:
+            query_api.fetch_recent_data()
+    return
+
+
+def _check_endpoint_param(
+    master_endpoints: t.List[dict], specific_endpoint_names: t.List[str] = None
+):
     # if specific_endpoint_names is None then retrieve all endpoints
     if not specific_endpoint_names:
-        _list_of_endpoint_dicts = LIST_OF_ENDPOINT_DICTS
+        _list_of_endpoint_dicts = master_endpoints
     else:
         ## Check if passed endpoints are valid, and if so add their dictionaries to _list_of_endpoint_dicts
         _list_of_endpoint_dicts = []
         ## Raise error if any of the endpoints are not in LIST_OF_ENDPOINT_DICTS
         dict_of_endpoints = {
             endpoint_dict.get("name"): endpoint_dict
-            for endpoint_dict in LIST_OF_ENDPOINT_DICTS
+            for endpoint_dict in master_endpoints
         }
 
         for endpoint_name in specific_endpoint_names:
@@ -39,17 +64,16 @@ def retrieve_data(
                     f"{endpoint_name} is not a valid endpoint\nAvailable endpoints: {dict_of_endpoints.keys()}"
                 )
             _list_of_endpoint_dicts.append(dict_of_endpoints.get(endpoint_name))
-
-    for endpoint_dict in _list_of_endpoint_dicts:
-        query_api = NBAAPI(endpoint_dict=endpoint_dict, all_dates=all_dates, test=test)
-        query_api.fetch_data()
-    return
+    return _list_of_endpoint_dicts
 
 
 class NBAAPI:
+    CURRENT_DATE = pd.to_datetime("today").normalize()
+
     def __init__(
         self,
         endpoint_dict,
+        load_type: str = "all",
         all_dates: bool = False,
         test: bool = False,
         data_path: str = None,
@@ -61,6 +85,9 @@ class NBAAPI:
         ----------
         endpoint_dict : dict
             Dictionary containing the parameters for the endpoint
+        load_type : str, optional
+            How much data to attempt to load, by default "all"
+            Other options are "yesterday" and "last_week"
         all_dates : bool, optional
             If True, retrieve all dates for the endpoint, by default False
         test : bool, optional
@@ -69,6 +96,7 @@ class NBAAPI:
             Path to where the data is stored, by default None
         """
         self.data_path = "./data/" if data_path is None else data_path
+        self.load_type = load_type
         self.start_season = (
             SEASONS_DICT.get("first_season")
             if all_dates
@@ -109,6 +137,10 @@ class NBAAPI:
         """
         try:
             data = pd.read_csv(self.current_csv_path)
+            if self.main_loop_parameter:
+                data[self.current_col] = data[self.current_col].fillna(
+                    data[self.main_loop_parameter]
+                )
             if self.main_col_dtype:
                 data[self.current_col] = data[self.current_col].astype(
                     self.main_col_dtype
@@ -144,7 +176,7 @@ class NBAAPI:
                 f"Error occurred while reading parent CSV file: {self.parent_csv_path}\n{exc}"
             ) from exc
 
-    def fetch_data(self):
+    def fetch_all_data(self):
         """
         Retrieve data from the NBA API and save it to a CSV file
         """
@@ -154,6 +186,38 @@ class NBAAPI:
             self._save_data(data)
             self._printer(verbage=f"Data saved for {self.name}")
             return
+
+        iterable = self._get_iterable()
+
+        for i, loop_value in enumerate(iterable):
+            if self.test and (i >= TEST_LOOPS):
+                break
+
+            if self.additional_parameters is None:
+                param_dict = {self.main_loop_parameter: loop_value}
+                data = self._call_endpoint(param_dict=param_dict)
+            else:
+                for j, param_dict in enumerate(
+                    self._get_distinct_loop_combos(loop_value)
+                ):
+                    if j == 0:
+                        data = self._call_endpoint(param_dict)
+                    else:
+                        data = pd.concat(
+                            [data, self._call_endpoint(param_dict)], ignore_index=True
+                        )
+            if data.empty:
+                self._printer(i=i, verbage=f"No data for {self.name}, {param_dict}")
+                continue
+            self._save_data(data)
+            self._printer(i=i, verbage=f"Data saved for {self.name}, {param_dict}")
+        return
+
+    def fetch_recent_data(self):
+        """
+        Retrieve data from the NBA API and save it to a CSV file
+        """
+        current_season_year = SEASONS_DICT.get("current_season_year")
 
         iterable = self._get_iterable()
 
@@ -209,9 +273,7 @@ class NBAAPI:
                 return self.endpoint()
             return self.endpoint(param_dict)
 
-        data_frame = retry_api_call(
-            api_call, endpoint=self.endpoint, param_dict=param_dict
-        )
+        data_frame = retry_api_call(api_call, param_dict=param_dict)
         data_frame = self._add_params(data_frame, param_dict)
         return data_frame
 
@@ -219,6 +281,11 @@ class NBAAPI:
         if self.save_transform:
             for col, transform in self.save_transform.items():
                 data[col] = data[col].apply(transform)
+
+        if self.main_loop_parameter and self.current_col not in data.columns:
+            ## Fill current column with the main loop parameter
+            data[self.current_col] = data[self.main_loop_parameter]
+
         if self.current_csv_data.empty:
             data.to_csv(self.current_csv_path, index=False)
             self.current_csv_data = self._load_current_csv()
@@ -239,9 +306,7 @@ class NBAAPI:
         if self.current_csv_data.empty:
             print(f"Getting all unique values of {self.parent_col}")
         else:
-            max_saved_loop_value = self.current_csv_data[self.current_col].max()
-            print(f"Filtering game dates to those greater than {max_saved_loop_value}")
-            loop_values = loop_values[loop_values > max_saved_loop_value]
+            loop_values = self._filter_loop_values(loop_values)
 
         if self.main_col_transform:
             loop_values = pd.Series(loop_values).apply(self.main_col_transform)
@@ -260,6 +325,42 @@ class NBAAPI:
             raise Exception(
                 f"Error occurred while getting unique values of {self.parent_col} from {self.parent_csv_path}\n{exc}"
             ) from exc
+
+    def _filter_loop_values(self, loop_values):
+        # max_saved_loop_value = self.current_csv_data[self.current_col].max()
+        # filtered_loop_values = loop_values[loop_values > max_saved_loop_value]
+        # return filtered_loop_values
+
+        if "date" in self.main_col_dtype:
+            if self.load_type == "yesterday":
+                loop_values = loop_values[
+                    loop_values >= self.CURRENT_DATE - pd.DateOffset(1)
+                ]
+            loop_values = pd.to_datetime(loop_values)
+        # if loop type is "all", find all values in parent loop that are not in current loop
+        if self.load_type == "all":
+            mask = np.isin(loop_values, self.current_csv_data[self.current_col])
+            filtered_loop_values = loop_values[~mask]
+            print(
+                f"Querying the {len(filtered_loop_values)} remaining loop values between {filtered_loop_values.min()} and {filtered_loop_values.max()}"
+            )
+            return filtered_loop_values
+        # if loop type is "today", find data for today's date, then save to current data (overwriting duplicate rows)
+        # This load type only applies to date specific data (e.g. game log, scoreboard, box score, etc.)
+        if self.load_type == "yesterday":
+            loop_values = loop_values[
+                loop_values >= self.CURRENT_DATE - pd.DateOffset(1)
+            ]
+            return loop_values
+        # if loop type is "today", find data for this week, then save to current data (overwriting duplicate rows)
+        # This load type only applies to date specific data (e.g. game log, scoreboard, box score, etc.)
+        if self.load_type == "last_week":
+            loop_values = loop_values[
+                (loop_values >= self.CURRENT_DATE - pd.DateOffset(days=7))
+            ]
+            return loop_values
+
+        return loop_values
 
     @staticmethod
     def _add_params(
