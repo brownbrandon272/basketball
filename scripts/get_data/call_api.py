112ddaf3d2a4db13d02930118d86449ac1d73193
Brandon Brown <brownbrandon272@gmail.com>
2023-09-13 18:09:38 +0100

    Making call_api more resilient

--- scripts/get_data/call_api.py
+++ scripts/get_data/call_api.py
@@ -8,24 +8,29 @@ from requests.exceptions import ReadTimeout
 from urllib3.exceptions import ProtocolError
 from json.decoder import JSONDecodeError
 
-MAX_RETRIES = 3
-WAIT_TIME = 40
+MAX_RETRIES = 5
+WAIT_TIME = 60
 
 
-def retry_api_call(api_call, max_retries=MAX_RETRIES, endpoint=None, param_dict=None):
+def retry_api_call(api_call, max_retries=MAX_RETRIES, param_dict=None):
     retries = 0
     while retries < max_retries:
         try:
             data = api_call()
-            # Add a random delay between 0.5 and 1 second
-            delay = random.uniform(0.5, 1.0)
+            # Add a random delay between 0.1 and 0.3 second
+            delay = random.uniform(0.1, 0.3)
             time.sleep(delay)
             return data
-        except (ConnectionError, ProtocolError, ReadTimeout, JSONDecodeError) as exc:
+        except (ConnectionError, ProtocolError, ReadTimeout) as exc:
             print(f"Error occurred: {exc}. param_dict: {param_dict}")
             retries += 1
             print(f"Retrying... (Attempt {retries}/{max_retries})")
             time.sleep(WAIT_TIME)
+        except JSONDecodeError as exc:
+            err_msg = f"Error occurred: {exc}"
+            print(f"{err_msg}. param_dict: {param_dict}")
+            param_dict["error"] = err_msg
+            return pd.DataFrame(index=[0], data=param_dict)
         except IndexError as exc:
             print(
                 f"Index Error occurred, possibly meaning that this API has no data for these params: {exc}"
@@ -33,5 +38,8 @@ def retry_api_call(api_call, max_retries=MAX_RETRIES, endpoint=None, param_dict=
             print(f"param_dict: {param_dict}")
             return pd.DataFrame()
         except Exception as exc:
-            raise exc
+            print(f"Unknown error occurred: {exc}")
+            retries += 1
+            print(f"Retrying... (Attempt {retries}/{max_retries})")
+            time.sleep(WAIT_TIME)
     raise ConnectionError(f"API call failed after {max_retries} retries.")
